dae833c194c40949cff2ceb438d2d87d6463f1f4
Atul Akella <atul.akella@gmail.com>
2024-07-04 00:34:40 +0530

    updated pub noted

--- src/arduinobot_py_examples/setup.py
+++ src/arduinobot_py_examples/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'simple_publisher = arduinobot_py_examples.simple_publisher:main',
         ],
     },
 )
