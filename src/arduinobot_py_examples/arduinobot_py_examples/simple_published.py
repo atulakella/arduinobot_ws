b814218d554ba768e52c9b398d0ba799e1d5080f
Atul Akella <atul.akella@gmail.com>
2024-07-04 00:20:15 +0530

    Edited pub

---
+++ src/arduinobot_py_examples/arduinobot_py_examples/simple_published.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.node import Node 
+
+class SimplePublisher(Node):
+    def __init__(self):
+        super().__init__('simple_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
