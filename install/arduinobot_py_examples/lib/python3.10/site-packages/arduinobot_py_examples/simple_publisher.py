78002d54f5982f40607df080c6dcc31621cafd9f
Atul Akella <atul.akella@gmail.com>
2024-07-31 07:26:57 +0530

    started making urdf

--- install/arduinobot_py_examples/lib/python3.10/site-packages/arduinobot_py_examples/simple_publisher.py
+++ install/arduinobot_py_examples/lib/python3.10/site-packages/arduinobot_py_examples/simple_publisher.py
@@ -1,29 +1,35 @@
 import rclpy
-from rclpy.node import Node 
+from rclpy.node import Node
 from std_msgs.msg import String
 
-class SimplePublisher(Node):
-    def __init__(self):
-        super().__init__('simple_publisher')
-        self.publisher_ = self.create_publisher(String, 'chatter', 10)
-        self.counter = 0
-        self.frequency = 1.0
-        self.get_logger().info('Publishing at %d Hz' % self.frequency)
-        self.timer = self.create_timer(self.frequency, self.timer_callback)
 
-    def timer_callback(self):
+class SimplePublisher(Node):
+
+    def __init__(self):
+        super().__init__("simple_publisher")
+        self.pub_ = self.create_publisher(String, "chatter", 10)
+        self.counter_ = 0
+        self.frequency_ = 1.0
+        self.get_logger().info("Publishing at %d Hz" % self.frequency_)
+        
+        self.timer_ = self.create_timer(self.frequency_, self.timerCallback)
+
+    def timerCallback(self):
         msg = String()
-        msg.data = 'Hello ROS2: %d' % self.counter
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.counter += 1
+        msg.data = "Hello ROS 2 - counter: %d" % self.counter_
+        self.pub_.publish(msg)
+        self.counter_ += 1
+
 
 def main():
     rclpy.init()
+
     simple_publisher = SimplePublisher()
     rclpy.spin(simple_publisher)
+    
     simple_publisher.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
-   main()
+    main()
