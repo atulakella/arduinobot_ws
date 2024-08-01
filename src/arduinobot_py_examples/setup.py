3788683a695ea1581260a69bf0b0d72c0cc8ebbd
Atul Akella <atul.akella@gmail.com>
2024-08-01 22:58:18 +0530

    Added simple parameter

--- src/arduinobot_py_examples/setup.py
+++ src/arduinobot_py_examples/setup.py
@@ -22,6 +22,7 @@ setup(
         'console_scripts': [
             'simple_publisher = arduinobot_py_examples.simple_publisher:main',
             'simple_subscriber = arduinobot_py_examples.simple_subscriber:main',
+            'simple_parameter = arduinobot_py_examples.simple_parameter:main',
         ],
     },
 )
