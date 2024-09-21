994c6dd946b4eba5eeb2af835618bed1651c0b86
Atul Akella <atul.akella@gmail.com>
2024-09-21 17:09:41 +0530

    Deleted service server and client

--- build/arduinobot_py_examples/prefix_override/sitecustomize.py
+++ build/arduinobot_py_examples/prefix_override/sitecustomize.py
@@ -1,4 +1,3 @@
 import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/atul/arduinobot_ws/install/arduinobot_py_examples'
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/atul/arduinobot_ws/install/arduinobot_py_examples'
